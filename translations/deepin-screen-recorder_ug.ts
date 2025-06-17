c51ab6a986714250de550884fdc639041d795b9f
ByteSupreme <bytesupreme.online@gmail.com>
2025-06-18 01:06:46 +0530

    Update deepin-screen-recorder_ug.ts

--- translations/deepin-screen-recorder_ug.ts
+++ translations/deepin-screen-recorder_ug.ts
@@ -501,6 +501,10 @@ select the area to record</source>
         <translation>30 fps</translation>
     </message>
     <message>
+        <source>60 fps</source>
+        <translation>60 fps</translation>
+    </message>
+    <message>
         <source>Save to</source>
         <translation>گە ساقلاش</translation>
     </message>
